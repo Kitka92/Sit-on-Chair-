77b37c225c994878b281c2ca16d55d0ec44bbaaa
Anita Swieconek <anita.swieconek92@gmail.com>
2017-01-24 20:51:20 +0100

    add js file

---
+++ js/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+  
+// SUBMENU
+  var oFirmie = document.querySelector(".menu li:first-child");
+  var arrow = document.querySelector(".arrow");
+  var submenu = document.querySelector(".submenu");
+  
+  arrow.classList.add("hidden");
+  submenu.classList.add("hidden");
+  
+  oFirmie.addEventListener("mouseover", function() {
+      arrow.classList.remove("hidden");
+      submenu.classList.remove("hidden");
+  });
+  
+// CENNIK
+  
+  var plan = document.querySelectorAll(".plan");
+  var topCircle = document.querySelectorAll(".topCircle");
+  var lilCircle = document.querySelectorAll(".lilCircle");
+  var basicPlan = document.querySelectorAll(".basicPlan");
+  var bigCircle = document.querySelectorAll(".bigCircle>span");
+  var bigCircleSup = document.querySelectorAll(".bigCircle>sup");
+  var odds = document.querySelectorAll(".options:nth-child(2n+1)");
+  var even = document.querySelectorAll(".options:nth-child(even)");
+  var buttons2 = document.querySelectorAll(".button2");
+  
+  for (var i = 0; i < plan.length; i++) {
+    plan[i].addEventListener("click", function() {
+      topCircle[i].style.backgroundColor = "pink";
+    });
+  }
+});
